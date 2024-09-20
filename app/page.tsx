e7703c34ead0515c7d3119f6e762aaacadc20fd5
Godwin Gino <godwingino10@gmail.com>
2024-09-21 02:46:11 +0530

    logo

--- app/page.tsx
+++ app/page.tsx
@@ -257,18 +257,6 @@ export default function Home() {
                 direction="left"
                 items={[
                   {
-                    logo: "/logo/magilogo.png",
-                    name: "Logo",
-                  },
-                  {
-                    logo: "/logo/figma.png",
-                    name: "Logo",
-                  },
-                  {
-                    logo: "/logo/google.png",
-                    name: "Logo",
-                  },
-                  {
                     logo: "/logo/logo.png",
                     name: "Logo",
                   },          
