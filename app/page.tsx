e6c4aca2b4d227658f8b0397a5f759ef5c84a8f0
Godwin Gino <godwingino10@gmail.com>
2024-09-20 11:20:25 +0530

    logo

--- app/page.tsx
+++ app/page.tsx
@@ -76,7 +76,7 @@ export default function Home() {
       >
         <Link href={"/"}>
           <Image
-            src={"./logo/logo.webp"}
+            src={"/logo/logo.webp"}
             alt="Logo"
             width={1000}
             height={1000}
@@ -263,6 +263,10 @@ export default function Home() {
                     logo: "/logo/magilogo.png",
                     name: "Logo",
                   },
+                  {
+                    logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png",
+                    name: "Logo",
+                  }
                 ]}
               />
             </section>
@@ -421,7 +425,7 @@ export default function Home() {
         <div className="flex flex-col  justify-between gap-y-3 xl:w-4/5 2xl:w-[68%] mx-auto">
           <h1 className="text-3xl md:text-5xl font-medium ">
             <Image
-              src={"./logo/logo.webp"}
+              src={"/logo/logo.webp"}
               width={10000}
               height={10000}
               className="w-40"
